edd6100333acc5c5ce9e0c091bea64b2b888004a
mllbll <zona904@yandex.ru>
2025-10-31 13:54:59 +0300

    Add Order Structure

---
+++ order/cmd/http_server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+const (
+	httpPort = "8080"
+	// Таймауты для HTTP-сервера
+	readHeaderTimeout = 5 * time.Second
+	shutdownTimeout   = 10 * time.Second
+)
+
+type OrderStorage struct {
+	mu       sync.RWMutex
+	order		map[string]*orderV1.Order
+}
