2547f88a079fc9459dca8b0dd48cbb4e5e4d965e
mllbll <zona904@yandex.ru>
2025-11-12 01:56:46 +0300

    Описан PaymentService

--- payment/cmd/main.go
+++ payment/cmd/main.go
@@ -1,5 +1,89 @@
 package main
 
 import (
-	paymentV1 
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	paymentV1 "github.com/mllbll/space-manufacture/shared/pkg/proto/payment/v1"
 )
+
+const grpcPort = 50051
+
+type paymentService struct {
+	paymentV1.UnimplementedPaymentServiceServer
+	// мапка со значениями transaction_uuid:payOrderMessage
+	mu               sync.RWMutex
+	payOrderMessages map[string]*paymentV1.PayOrderMessage
+}
+
+func (s *paymentService) Pay(_ context.Context, req *paymentV1.PayOrderRequest) (*paymentV1.PayOrderResponse, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	newTransactionUUID := uuid.New().String()
+
+	newPayOrderMessage := &paymentV1.PayOrderMessage{
+		OrderUuid:     req.PayOrderMessage.OrderUuid,
+		UserUuid:      req.PayOrderMessage.UserUuid,
+		PaymentMethod: req.PayOrderMessage.PaymentMethod,
+	}
+
+	s.payOrderMessages[newTransactionUUID] = newPayOrderMessage
+
+	log.Printf("Оплата прошла успешно, transaction_uuid: %s", newTransactionUUID)
+
+	return &paymentV1.PayOrderResponse{
+		TransactionUuid: newTransactionUUID,
+	}, nil
+
+}
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":d", grpcPort))
+	if err != nil {
+		log.Printf("failed to listen: %v\n", err)
+		return
+	}
+
+	defer func() {
+		if cerr := lis.Close(); cerr != nil {
+			log.Printf("failed to close listener: %v\n", cerr)
+		}
+	}()
+
+	s := grpc.NewServer()
+
+	service := &paymentService{
+		payOrderMessages: make(map[string]*paymentV1.PayOrderMessage),
+	}
+
+	paymentV1.RegisterPaymentServiceServer(s, service)
+
+	reflection.Register(s)
+
+	go func() {
+		log.Printf("gRPC server listening on %d\n", grpcPort)
+		err = s.Serve(lis)
+		if err != nil {
+			log.Printf("failed to serve %v\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Printf("Shutting down gRPC server ...")
+	s.GracefulStop()
+	log.Printf("Server Stopped")
+
+}
---
+++ payment/go.mod
@@ -0,0 +1,17 @@
+module payment
+
+go 1.25.1
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/mllbll/space-manufacture/shared v0.0.0-20251111212116-899e62c949af
+)
+
+require (
+	golang.org/x/net v0.44.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250804133106-a7a43d27e69b // indirect
+	google.golang.org/grpc v1.76.0 // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
+)
